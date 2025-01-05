744c657ce0fdd6c00a91e80d6975398a3ece3dcc
Chethana Dilhari <chethanadilhari99@gmail.com>
2025-01-05 15:09:34 +0530

    Install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}', // Add this line to include JSX files
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
